d640b9f9603a078c24525c2312a942cf0759020a
Nat <nat@cmmc.io>
2017-05-24 05:31:35 +0700

    add 002

---
+++ 002_cmmc_mqtt_esp8266/002_cmmc_mqtt_esp8266/001_cmmc_mqtt_esp8266.ino
@@ -0,0 +1,116 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include "CMMC_Blink.hpp"
+CMMC_Blink blinker;
+#define relayPin 15
+
+// Update these with values suitable for your network.
+const char* ssid = "ESPERT-3020";
+const char* password = "espertap";
+const char* mqtt_server = "mqtt.cmmc.io";
+
+#define inTopic "CMMC/plug001"
+#define outTopic "CMMC/plug001/outTopic"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  pinMode(relayPin, OUTPUT);
+  Serial.begin(115200);
+  blinker.init();
+  blinker.blink(200, LED_BUILTIN);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  blinker.blink(50, LED_BUILTIN);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    digitalWrite(relayPin, HIGH);
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+    digitalWrite(relayPin, LOW);
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      blinker.detach(HIGH);
+      // Once connected, publish an announcement...
+      client.publish(outTopic, "hello world");
+      // ... and resubscribe
+      client.subscribe(inTopic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    blinker.blink(50, LED_BUILTIN);
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    ++value;
+    snprintf (msg, 75, "hello world #%ld", value);
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish(outTopic, msg);
+  }
+}
